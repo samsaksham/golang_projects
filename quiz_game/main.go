3d5a796510a2bac1239f6e1bbb498f41149eb73d
samsaksham <rajput.saksham29@gmail.com>
2024-01-06 22:14:05 +0530

    Day 1 2 part

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -5,56 +5,77 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
-	csvFileName := flag.String("csv","problems.csv","A csv file for questions")
+	csvFileName := flag.String("csv", "problems.csv", "A csv file for questions")
+	timeLimet := flag.Int("limit", 30, "Time Limit for QUIZ")
 	flag.Parse()
 	
-	
-	file,err := os.Open(*csvFileName)
-	if err != nil{
-		exit(fmt.Sprintf("Failed to open the csv file %s \n",*csvFileName))
-		
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the csv file %s \n", *csvFileName))
+
 	}
 
 	r := csv.NewReader(file)
-	lines,err := r.ReadAll()
-	if err != nil{
+	lines, err := r.ReadAll()
+	if err != nil {
 		exit(fmt.Sprintf("CSV IS Invalid "))
 	}
 	problems := readLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimet) * time.Second)
+
 	
+
 	cnt := 0
-	for i,p := range problems{
-		fmt.Printf("The Question number %d is %s = ",i+1,p.q)
+	for i, p := range problems {
+		fmt.Printf("The Question number %d is %s = ", i+1, p.q)
+		anschan := make(chan string)
 
-		var ans string 
-		
-		fmt.Scanf("%s\n",&ans)
+		go func ()  {
+			var answer string
 
-		if ans == p.a{
-			fmt.Printf("Corrent Answer \n")
-			cnt++
+			fmt.Scanf("%s\n", &answer)
+
+			anschan <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("You Scored ", cnt)
+			return
+
+		case answer := <- anschan:
+			
+
+			if answer == p.a {
+				fmt.Printf("\nCorrent Answer \n")
+				cnt++
+			}
+
+
 		}
-	
+		
+		
+
 	}
 
-	fmt.Println("You Scored ",cnt)
-
-
+	
 
 }
 
 func readLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 
-    for i, line := range lines{
-	ret[i] = problem{
-		q : line[0],
-		a : line[1],
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
 	}
-}
 
 	return ret
 }
@@ -64,8 +85,7 @@ type problem struct {
 	a string
 }
 
-
-func exit(msg string){
+func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
 }
